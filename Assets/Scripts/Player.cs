6b031bd896b915fb188ef78238d0df53181f6211
jsk <jsk07aa@gmail.com>
2023-05-24 09:56:15 +0900

    05/24_2
    Player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float force = 1;
+    public float force;
+    public float maxSpeed;
     public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -15,7 +16,24 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        rb.AddForce(new Vector3(0, v, 0) * force);
+        rb.AddForce(new Vector3(h, v, 0) * force);
+
+    }
+
+    private void FixedUpdate()
+    {
+        // 현재 속도 벡터를 가져옵니다.
+        Vector3 velocity = rb.velocity;
+
+        // 속도 벡터의 길이가 최대 속도보다 큰 경우, 길이를 최대 속도로 제한합니다.
+        if (velocity.magnitude > maxSpeed)
+        {
+            velocity = velocity.normalized * maxSpeed;
+        }
+
+        // 제한된 속도 벡터를 다시 Rigidbody에 적용합니다.
+        rb.velocity = velocity;
     }
 }
