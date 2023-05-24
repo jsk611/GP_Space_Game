778ef6f2aa9561617cea12f0f5f98cab74d97a8d
jsk <jsk07aa@gmail.com>
2023-05-24 10:22:38 +0900

    05/24_3

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,16 +24,13 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // 현재 속도 벡터를 가져옵니다.
         Vector3 velocity = rb.velocity;
 
-        // 속도 벡터의 길이가 최대 속도보다 큰 경우, 길이를 최대 속도로 제한합니다.
         if (velocity.magnitude > maxSpeed)
         {
             velocity = velocity.normalized * maxSpeed;
         }
 
-        // 제한된 속도 벡터를 다시 Rigidbody에 적용합니다.
         rb.velocity = velocity;
     }
 }
