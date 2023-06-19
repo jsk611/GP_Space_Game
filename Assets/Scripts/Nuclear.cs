82bff5ff2b5d6873b60f65605a8c22f8b5d8ddf6
jsk <jsk07aa@gmail.com>
2023-06-19 10:08:49 +0900

    nuclear

---
+++ Assets/Scripts/Nuclear.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nuclear : MonoBehaviour
+{
+    public int target;
+    [SerializeField] float speed;
+    [SerializeField] GameObject explosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target == 1)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 90f);
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+        else if (target == 2)
+        {
+            transform.eulerAngles = new Vector3(0, 0, -90f);
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if(other.gameObject.name == "Planet" + target.ToString())
+        {
+            Instantiate(explosion, transform.position, Quaternion.Euler(0, 0, GetAngle(other.transform.position, transform.position) - 90f));
+
+            other.GetComponent<Planet>().Hp -= 15f;
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Meteor"))
+        {
+
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    float GetAngle(Vector2 start, Vector2 end)
+    {
+        Vector2 v2 = end - start;
+        return Mathf.Atan2(v2.y, v2.x) * Mathf.Rad2Deg;
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -12,6 +13,10 @@ public class Player : MonoBehaviour
     [SerializeField] Player2Move p2m;
     float h = 0;
     float v = 0;
+
+    [SerializeField] Image mf;
+    bool hasCoil;
+    [SerializeField] GameObject nuclear;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,37 +26,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //h = 0; v = 0;
-
-        //#region MovingKeySetting
-        //if (Input.GetKey(KeyCode.D))
-        //{
-        //    h = 1;
-        //}
-        //else if (Input.GetKey(KeyCode.A))
-        //{
-        //    h = -1;
-        //}
-        //if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A))
-        //{
-        //    h = 0;
-        //}
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    v = 1;
-        //}
-        //if (Input.GetKey(KeyCode.S))
-        //{
-        //    v = -1;
-        //}
-        //if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S))
-        //{
-        //    v = 0;
-        //}
-        //#endregion
-
-        //rb.AddForce(new Vector3(h, v, 0) * force);
-
         if (rb.velocity.x > 0) transform.localScale = new Vector3(0.5f, 0.5f, 1f);
         if (rb.velocity.x < 0) transform.localScale = new Vector3(0.5f, -0.5f, 1f);
     }
@@ -70,20 +44,49 @@ public class Player : MonoBehaviour
 
     IEnumerator CoilEffect()
     {
+        hasCoil = true;
+
         rb.mass = 100f;
+        mf.color = new Color(1, 0.75f, 0, 40f/255);
+
         if (p1m != null) p1m.force = 8000f;
         if (p2m != null) p2m.force = 8000f;
+
         yield return new WaitForSeconds(10f);
+
         rb.mass = 50f;
+        mf.color = new Color(140f/255,140f/255,140f/255, 40f / 255);
+
         if (p1m != null) p1m.force = 4000f;
         if (p2m != null) p2m.force = 4000f;
+
+        hasCoil = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Coil"))
         {
-            StartCoroutine(CoilEffect());
+            if (!hasCoil)
+            {
+                StartCoroutine(CoilEffect());
+                Destroy(other.gameObject);
+            }
+        }
+
+        if (other.gameObject.CompareTag("Missile"))
+        {
+            if(p2m == null)//자신이 플레이어 1일때
+            {
+                Nuclear n = Instantiate(nuclear, new Vector3(-19f, Random.Range(-7.5f, 7.5f), 0), Quaternion.identity).GetComponent<Nuclear>();
+                n.target = 2;
+            }
+
+            else if(p1m == null)//자신이 플레이어 2일때
+            {
+                Nuclear n = Instantiate(nuclear, new Vector3(19f, Random.Range(-7.5f, 7.5f), 0), Quaternion.identity).GetComponent<Nuclear>();
+                n.target = 1;
+            }
             Destroy(other.gameObject);
         }
     }
