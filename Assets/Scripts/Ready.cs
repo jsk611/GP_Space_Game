d43b77aaf038e179e0427bc7d31153888a8a7020
김진현 <qwekimjh@gmail.com>
2023-06-26 09:47:43 +0900

    UI

--- Assets/Scripts/Ready.cs
+++ Assets/Scripts/Ready.cs
@@ -7,6 +7,7 @@ public class Ready : MonoBehaviour
 {
     public GameObject btn, rb1, rb2;
     Toggle t1, t2;
+    public Text txt1, txt2;
     void Start()
     {
         Screen.SetResolution(1920 / 2, 1080 / 2, false);
@@ -17,5 +18,9 @@ public class Ready : MonoBehaviour
     {
         if(t1.isOn == true && t2.isOn == true) btn.SetActive(true);
         else btn.SetActive(false);
+        if(t1.isOn == true) txt1.color = new Color(73f/255f, 127f/255f, 93f/255f);
+        else txt1.color = new Color(143f/255f, 1f, 186f/255f);
+        if(t2.isOn == true) txt2.color = new Color(73f/255f, 127f/255f, 93f/255f);
+        else txt2.color = new Color(143f/255f, 1f, 186f/255f);
     }
 }
